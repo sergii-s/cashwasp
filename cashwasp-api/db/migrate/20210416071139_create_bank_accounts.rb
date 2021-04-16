9c0e9700bda743a9e2e7fffd59e64323e035912c
sergii <sergii@antvoice.com>
2021-04-16 09:07:32 +0200

    create models

---
+++ cashwasp-api/db/migrate/20210416071139_create_bank_accounts.rb
@@ -0,0 +1,12 @@
+class CreateBankAccounts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :bank_accounts do |t|
+      t.string :label
+      t.string :holder
+      t.string :type
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ cashwasp-api/db/schema.rb
@@ -0,0 +1,33 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_04_16_071139) do
+
+  create_table "bank_accounts", force: :cascade do |t|
+    t.string "label"
+    t.string "holder"
+    t.string "type"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_bank_accounts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "bank_accounts", "users"
+end
